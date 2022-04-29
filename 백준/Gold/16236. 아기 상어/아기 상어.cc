a0237591cbe1e6085704eccc2593bed67e7f3506
Jinho Ryu <37369443+ryuj-h@users.noreply.github.com>
2022-04-29 21:47:35 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -2,188 +2,131 @@
 #include<iostream>
 #include <cstring>
 #include <queue>
-
 using namespace std;
 
-typedef struct {
-	int size;
-	int x;
-	int y;
-	int exp;
-}shark;
+int dirx[] = { 0,0,-1,1};
+int diry[] = { -1,1,0,0};
 
-typedef struct {
-	int x;
-	int y;
-}point;
-
-typedef struct {
-	int x;
-	int y;
-	int distance;
-}result;
-
-int dirx[] = { 0,-1,1,0};
-int diry[] = { -1,0,0,1};
-
+/* 판과 방문 관련 변수 */
 int N , M;
 int board[21][21];
-int copyboard[21][21];
 int visit[21][21];
 int visitnum[21][21];
 
+/* 아기 상어 변수 */
+int Babyx;
+int Babyy;
+int Babyexp;
+int Babylevel;
 
-int POINT = 0;
-shark baby;
+/* 가장 적합한 위치 정보 */
+int Magicx;
+int Magicy;
+int Magicdist;
 
-queue<point> q;
+/* 최종 시간 */
+int TIME = 0;
 
-void print()
+queue<std::pair<int, int>> q;
+
+void initBFS()
 {
-	cout << endl;
-	for (int y = 0; y < N; y++)
-	{
-		for (int x = 0; x < N; x++)
-		{
-			cout << board[y][x] << "	";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
+	Magicx = 20;
+	Magicy = 20;
+	Magicdist = 400;
 
-bool BFS(int x,int y , result *res,bool eat = false)
-{
 	memset(visit, 0, sizeof(visit));
 	memset(visitnum, 0, sizeof(visitnum));
-	memcpy(copyboard, board, sizeof(board));
+}
 
-	point start{ baby.x, baby.y };
-	q.push(start);
-
-	visitnum[baby.y][baby.x] = 0;
-	visit[baby.y][baby.x] = 1;
+void BFS(int startx, int starty)
+{
+	q.push(make_pair(startx, starty));
+	visit[starty][startx] = 1;
 
 	while (!q.empty())
 	{
-		point currpos = q.front();
+		auto a = q.front();
+		int currx = a.first;
+		int curry = a.second;
 		q.pop();
-
-		if (currpos.x == x && currpos.y == y)
-		{
-			res->x = x;
-			res->y = y;
-			res->distance = visitnum[y][x];
-
-			if (!eat)
-			{
-				memcpy(board, copyboard, sizeof(board));
-			}
-			if (eat)
-			{
-				board[baby.y][baby.x] = 0;
-				board[y][x] = 9;
-				baby.x = x;
-				baby.y = y;
-				baby.exp++;
-				if (baby.exp == baby.size)
-				{
-					baby.size++;
-					baby.exp = 0;
-				}
-			}
-
-			while (!q.empty())
-				q.pop();
-			return true;
-		}
 
 		for (int d = 0; d < 4; d++)
 		{
-			int nextx = currpos.x + dirx[d];
-			int nexty = currpos.y + diry[d];
+			int nextx = currx + dirx[d];
+			int nexty = curry + diry[d];
 
-			if (nextx < 0 || nextx == N || nexty < 0 || nexty == N)
+			if (nextx < 0 || nextx >= N || nexty < 0 || nexty >= N)
 				continue;
 			if (visit[nexty][nextx] == 1)
 				continue;
-			if (board[nexty][nextx] > baby.size)
+			if (board[nexty][nextx] > Babylevel)//아기 상어는 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없고, 나머지 칸은 모두 지나갈 수 있다. 
 				continue;
 
-
 			visit[nexty][nextx] = 1;
-			visitnum[nexty][nextx] = visitnum[currpos.y][currpos.x] + 1;
-			point next{ nextx,nexty };
+			visitnum[nexty][nextx] = visitnum[curry][currx] + 1;
 
-			q.push(next);
-		}
-	}
-
-	memcpy(board, copyboard, sizeof(board));
-	return false;
-}
-
-
-
-void solve()
-{
-	while (true)
-	{
-		int eatablenum = 0;
-		int selectedX = 99999999;
-		int selectedY = 99999999;
-		int selectedDist = 99999999;
-
-		for (int y = 0; y < N; y++) {
-			for (int x = 0; x < N; x++) {
-				if (board[y][x] != 0 && board[y][x] < baby.size && board[y][x] !=9)
+			if (board[nexty][nextx] >= 1 && board[nexty][nextx] <= 6)//현재 좌표가 물고기라면
+			{
+				if (Babylevel > board[nexty][nextx])//아기 상어는 자신의 크기보다 작은 물고기만 먹을 수 있다.
 				{
-					result res;
-					bool can = BFS(x, y, &res);
-					if (can)
+					if (Magicdist > visitnum[nexty][nextx])//첫번째로 거리가 더 짧은 좌표면 변경
 					{
-						eatablenum++;
-						if (selectedDist > res.distance)
+						Magicx = nextx;
+						Magicy = nexty;
+						Magicdist = visitnum[nexty][nextx];
+					}
+					else if (Magicdist == visitnum[nexty][nextx])//거리가 같다면
+					{
+						if (Magicy > nexty)//행이 더 작은거로(위 쪽에 있는 것)
 						{
-							selectedX = res.x;
-							selectedY = res.y;
-							selectedDist = res.distance;
+							Magicx = nextx;
+							Magicy = nexty;
+							Magicdist = visitnum[nexty][nextx];
 						}
-						else if (selectedDist == res.distance)
+						else if (Magicy == nexty)//행도 같다면
 						{
-							if (selectedY > res.y)
+							if (Magicx > nextx)//열이 작은걸로 (왼 쪽에있는것)
 							{
-								selectedX = res.x;
-								selectedY = res.y;
-								selectedDist = res.distance;
-							}
-							else if (selectedY == res.y)
-							{
-								if (selectedX > res.x)
-								{
-									selectedX = res.x;
-									selectedY = res.y;
-									selectedDist = res.distance;
-								}
+								Magicx = nextx;
+								Magicy = nexty;
+								Magicdist = visitnum[nexty][nextx];
 							}
 						}
 					}
 				}
 			}
-		}
-
-		if (eatablenum == 0)
-		{
-			break;
-		}
-		else
-		{
-			result res;
-			BFS(selectedX, selectedY, &res, true);
-			POINT = POINT + selectedDist;
+			q.push(make_pair(nextx, nexty));
 		}
 	}
 }
+
+void solve()
+{
+	while (true)
+	{
+		initBFS();
+		BFS(Babyx, Babyy);
+
+		if (Magicx == 20 && Magicy == 20 && Magicdist == 400)//해를 찾지 못했음. 와일문 종료
+			break;
+		else
+		{
+			Babyx = Magicx;
+			Babyy = Magicy;
+			board[Babyy][Babyx] = 0;
+			Babyexp++;
+
+			if (Babyexp == Babylevel)
+			{
+				Babylevel++;
+				Babyexp = 0;
+			}
+			TIME = TIME + Magicdist;
+		}
+	}
+}
+
 
 int main(int argc, char** argv)
 {
@@ -193,16 +136,17 @@ int main(int argc, char** argv)
 			cin >> board[y][x];
 			if (board[y][x] == 9)
 			{
-				baby.size = 2;
-				baby.x = x;
-				baby.y = y;
+				board[y][x] = 0;
+				Babylevel = 2;
+				Babyexp = 0;
+				Babyx = x;
+				Babyy = y;
 			}
 		}
 	}
+	
 	solve();
-
-
-	cout << POINT << endl;
+	cout << TIME << endl;
 
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
