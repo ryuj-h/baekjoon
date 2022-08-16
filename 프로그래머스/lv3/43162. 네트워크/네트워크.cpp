1ffc308ef3d033f5deb18d243f8c5f92f50e34c5
Jinho Ryu <37369443+ryuj-h@users.noreply.github.com>
2022-08-16 18:33:15 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+
+
+
+using namespace std;
+
+int g_N;
+bool g_visit[201];
+
+void bfs(int index,vector<vector<int>> computers)
+{
+    queue<int> q;
+    g_visit[index] = true;
+    q.push(index);
+    
+    while(!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+        for(int i = 0;i< g_N;i++)
+        {
+            if (i == curr)
+                continue;
+            if(computers[curr][i] == 1 && g_visit[i] == false)
+            {
+                g_visit[i] = true;
+                q.push(i);
+            }
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    g_N = n;
+    memset(g_visit, false,sizeof(g_visit));
+    
+    for(int i= 0;i<g_N;i++)
+    {
+        if(g_visit[i] == false)
+        {
+            bfs(i,computers);
+            answer++;
+        }
+    }
+    
+    return answer;
+}
