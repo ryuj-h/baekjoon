1f62e823cfcecfc4094324ca648dcc8541e6fa73
Jinho Ryu <37369443+ryuj-h@users.noreply.github.com>
2023-10-10 17:26:32 +0900

    [Silver III] Title: N과 M (2), Time: 212 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,36 +1,40 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int N, M;
-	static StringBuilder sb = new StringBuilder();
-	static int picked[];
-	static boolean visited[];
+
+	static int N,M;
+	static int []arr;
+	static StringBuilder sb=new StringBuilder();
 	
-	public static void comb(int cnt, int start) {
-		if(cnt == M) {
-			for(int i =0; i < M;i++) {
-				sb.append(picked[i] + " ");		
-			}
-			sb.append("\n");
-		}
-		for(int i = start; i < N; i++) {
-			if (visited[i]) continue;
-			
-			visited[i] = true;
-			picked[cnt] = i + 1;
-			comb(cnt + 1, i + 1);
-			visited[i] = false;
-		}
-	}
-	
-	
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		visited = new boolean [N];
-		picked = new int[N];
-		comb(0, 0);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		N=sc.nextInt();
+		M=sc.nextInt();
+		arr=new int[M];
+		// 숫자 범위는 1~N
+		// 그 중 중복없이 M개를 뽑으며 순서 고려X -> start값이 계속 바뀌게됨
+		
+		// 입력값 arr 안쓰고 for문 변수로 해도 될듯?
+		recursive(0, 1);
 		System.out.println(sb);
 	}
+	static void recursive(int idx, int start) {
+		// 종료 조건
+		if(idx==M) {
+			for(int i=0; i<M; i++) {
+				sb.append(arr[i]).append(' ');
+			}
+			sb.append('\n');
+//			System.out.println(Arrays.toString(arr));
+			return;
+		}
+		for(int i=start; i<=N; i++) {
+			//start~N
+			arr[idx]=i;
+			recursive(idx+1, i+1);
+		}
+	}
+
 }
